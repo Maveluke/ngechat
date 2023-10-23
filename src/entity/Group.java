930c890e8c0359cc586cebac06a13b0a9d4e88cb
timothym4r <130308583+timothym4r@users.noreply.github.com>
2023-10-23 17:39:10 -0400

    adding entities

---
+++ src/entity/Group.java
@@ -0,0 +1,9 @@
+package entity;
+
+public interface Group {
+
+    public void addUsers(User user);
+
+
+
+}
---
+++ src/entity/User.java
@@ -0,0 +1,12 @@
+package entity;
+
+public interface User {
+
+    public String getName();
+
+    void changeName(String name);
+
+    public String getID();
+
+
+}
