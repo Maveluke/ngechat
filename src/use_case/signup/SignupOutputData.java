0b3f5b4c952da5917caf5ecb5f1f1bbe58c9ac99
Maveluke <maveluk.cy@gmail.com>
2023-11-03 14:03:44 -0400

    Adding SignupUseCaseFactory and use case for Sign Up

---
+++ src/use_case/signup/SignupOutputData.java
@@ -0,0 +1,27 @@
+package use_case.signup;
+
+public class SignupOutputData {
+
+    private final String username;
+    private String creationTime;
+
+    private boolean useCaseFailed;
+
+    public SignupOutputData(String username, boolean useCaseFailed) {
+        this.username = username;
+        this.useCaseFailed = useCaseFailed;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getCreationTime() {
+        return creationTime;
+    }
+
+    public void setCreationTime(String creationTime) {
+        this.creationTime = creationTime;
+    }
+
+}
