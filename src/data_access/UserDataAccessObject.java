6dd59e10bd92acf0b561f9ccdf4b945fa2c801f0
timothym4r <130308583+timothym4r@users.noreply.github.com>
2023-11-06 17:30:13 -0500

    Commit login use_case and LoginController

--- src/data_access/UserDataAccessObject.java
+++ src/data_access/UserDataAccessObject.java
@@ -1,6 +1,14 @@
 package data_access;
 
+import entity.User;
+
+import java.util.HashMap;
+
 public class UserDataAccessObject {
 
+    private HashMap<String, User> Users;
+
+
+    public UserDataAccessObject() {
     
 }
--- src/entity/CommonUser.java
+++ src/entity/CommonUser.java
@@ -1,5 +1,9 @@
 package entity;
 
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Random;
+
 public class CommonUser implements User {
 
         private String Username;
@@ -7,28 +11,57 @@ public class CommonUser implements User {
 
         private final String Password;
 
-        public CommonUser(String username, String Id, String password){
-                this.Username = username;
-                this.Id = Id;
-                this.Password = password;
+
+                private static String randomInt() {
+                        Random random = new Random();
+
+                        // defining the lower and upper bounds
+
+                        int lowerBound = 10;
+                        int upperBound = 100;
+                        HashMap<String, User> hashMap = ;
+                        Collection<User> valuesCollection = hashMap.values();
+                        // Generate a random integer between lowerBound (inclusive) and upperBound (exclusive)
+                        while (true) {
+                                int randomInt = random.nextInt(upperBound - lowerBound) + lowerBound;
+                                for (User value : valuesCollection) {
+                                        if (Integer.parseInt(value.getID()) == randomInt) {
+                                                break;
+                                        }
+                                        return Integer.toString(randomInt);
+                                }
 
 
-        }
+                                }
+                        }
 
-        public String getName(){
-                return Username;
-        }
+                        public CommonUser(String username, String password) {
+                                this.Username = username;
+                                this.Password = password;
+                                this.Id = randomInt();
 
-        @Override
-        public void changeName(String name) {
-                this.Username = name;
-        }
 
-        public String getID(){
-                return this.Id;
-        }
+                        }
 
-        public String getPassword() {return this.Password;}
 
-        }
+                        public String getName () {
+                                return Username;
+                        }
+
+                        @Override
+                        public void changeName (String name){
+                                this.Username = name;
+                        }
+
+                        public String getID () {
+                                return Id;
+                        }
+
+                        public String getPassword () {
+                                return this.Password;
+                        }
+
+                }
+
+
 
