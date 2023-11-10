383d879044dceda5fc4ff7867e04c2854dc2f2eb
Maveluke <maveluk.cy@gmail.com>
2023-11-10 01:19:25 -0500

    Added an attribute to keep track of friends

--- src/entity/CommonUser.java
+++ src/entity/CommonUser.java
@@ -1,10 +1,12 @@
 package entity;
 
+import java.util.ArrayList;
+
 public class CommonUser implements User {
 
         private String Username;
         private final String Id;
-
+        private final ArrayList<User> friendsList = new ArrayList<>();
         private final String Password;
 
         public CommonUser(String username, String Id, String password){
@@ -28,5 +30,16 @@ public class CommonUser implements User {
 
         public String getPassword() {return this.Password;}
 
+        public ArrayList<User> getFriendsList() {
+                return new ArrayList<>(friendsList);
         }
+        @Override
+        public boolean userAddFriend(User friend){
+                return friendsList.add(friend);
+        }
+        @Override
+        public boolean userRemoveFriend(User friend){
+                return friendsList.remove(friend);
+        }
+}
 
