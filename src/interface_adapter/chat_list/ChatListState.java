0f3236f421fbed500bb698cef2321f1fbf4cbf33
Maveluke <maveluk.cy@gmail.com>
2023-11-26 13:50:12 -0500

    Implemented ChatListState

--- src/interface_adapter/chat_list/ChatListState.java
+++ src/interface_adapter/chat_list/ChatListState.java
@@ -1,5 +1,28 @@
 package interface_adapter.chat_list;
 
-public class ChatListState {
+import java.util.ArrayList;
+import java.util.HashMap;
 
+public class ChatListState {
+    private String username = "Username";
+
+    private HashMap<String, ArrayList<String>> chatList = new HashMap<String, ArrayList<String>>();
+
+    public ChatListState(){};
+
+    public String getUsername() {
+        return username;
+    }
+
+    public HashMap<String, ArrayList<String>> getChatList() {
+        return chatList;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public void setChatList(HashMap<String, ArrayList<String>> chatList) {
+        this.chatList = chatList;
+    }
 }
