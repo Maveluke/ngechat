90bf987a18b2df316d477a1777cadcc0b4c68bd0
timothym4r <130308583+timothym4r@users.noreply.github.com>
2023-11-26 17:18:15 -0500

    Creating a ChatListDataAccessObject.java

--- src/data_access/ChatListDataAccessObject.java
+++ src/data_access/ChatListDataAccessObject.java
@@ -2,20 +2,17 @@ package data_access;
 
 import entity.User;
 import use_case.chat_list.ChatListDataAccessInterface;
+import use_case.create_chat.CreateChatDataAccessInterface;
 
 import java.util.ArrayList;
 import java.util.HashMap;
 
-public class ChatListDataAccessObject implements ChatListDataAccessInterface {
+public class ChatListDataAccessObject implements ChatListDataAccessInterface, CreateChatDataAccessInterface {
 
+    private String currentUsername;
 
     @Override
     public HashMap<String, ArrayList<String>> getChats() {
-        return null;
-    }
-
-    @Override
-    public User getCurrentUser() {
         return null;
     }
 
@@ -26,6 +23,16 @@ public class ChatListDataAccessObject implements ChatListDataAccessInterface {
 
     @Override
     public void deleteChat() {
-
     }
+
+    @Override
+    public boolean chatExist(String targetUser) {
+        for (String key : getChats().keySet()) {
+            if (key.equals(targetUser)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
 }
