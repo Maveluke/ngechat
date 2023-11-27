4d281b2ff458dd7f5cdcc42fd6e7d52562f7a15b
Maveluke <maveluk.cy@gmail.com>
2023-11-26 19:27:14 -0500

    Modified such that ChatListDAO receives friendToBin map

--- src/use_case/chat_list/ChatListInteractor.java
+++ src/use_case/chat_list/ChatListInteractor.java
@@ -1,15 +1,19 @@
 package use_case.chat_list;
 
+import entity.User;
+import use_case.login.LoginDataAccessInterface;
+
 import java.util.ArrayList;
 import java.util.HashMap;
 
 public class ChatListInteractor implements ChatListInputBoundary{
 
-
+    private LoginDataAccessInterface userDataAccessObject;
     private ChatListDataAccessInterface chatListDataAccessObject;
     private ChatListOutputBoundary chatListPresenter;
 
-    public ChatListInteractor(ChatListDataAccessInterface chatListDataAccessObject, ChatListOutputBoundary chatListPresenter){
+    public ChatListInteractor(LoginDataAccessInterface userDataAccessObject, ChatListDataAccessInterface chatListDataAccessObject, ChatListOutputBoundary chatListPresenter){
+        this.userDataAccessObject = userDataAccessObject;
         this.chatListDataAccessObject = chatListDataAccessObject;
         this.chatListPresenter = chatListPresenter;
     }
@@ -19,6 +23,11 @@ public class ChatListInteractor implements ChatListInputBoundary{
         if (chatListDataAccessObject.is_empty()) {
             chatListPresenter.prepareFailView("No chat available");
         } else {
+            HashMap<User, String> userFriendTobinID = userDataAccessObject.getCurrentUser().getFriendToBinMap();
+            for (User friendUser :
+                    userFriendTobinID.keySet()) {
+                chatListDataAccessObject.addFriendTobinID(friendUser.getName(), userFriendTobinID.get(friendUser));
+            }
             HashMap<String, ArrayList<String>> chatList = chatListDataAccessObject.getChats();
             ChatListOutputData chatListOutputData = new ChatListOutputData(chatList);
             chatListPresenter.prepareSuccessView(chatListOutputData);
