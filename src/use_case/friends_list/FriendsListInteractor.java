22315b3e9982f40f5a55a1ce043ce58b0214c8d1
Maveluke <maveluk.cy@gmail.com>
2023-11-30 17:39:05 -0500

    Change the return type of getFriends

--- src/use_case/friends_list/FriendsListInteractor.java
+++ src/use_case/friends_list/FriendsListInteractor.java
@@ -3,6 +3,7 @@ package use_case.friends_list;
 import entity.User;
 import use_case.chat_list.ChatListOutputData;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 
 public class FriendsListInteractor implements FriendsListInputBoundary {
@@ -20,7 +21,7 @@ public class FriendsListInteractor implements FriendsListInputBoundary {
 
             User currentUser = friendsListDataAccessObject.getCurrentUser();
             String currentUserName = currentUser.getName();
-            HashMap<String, String> friendsList = friendsListDataAccessObject.getFriends();
+            ArrayList<String> friendsList = friendsListDataAccessObject.getFriends();
 
             FriendsListOutputData friendsListOutputData = new FriendsListOutputData(friendsList, currentUserName);
             friendsListPresenter.prepareSuccessView(friendsListOutputData);
