988d2f9b214025b0dc5f662aef12198986da46d6
Newtella3105 <melissacb3105@gmail.com>
2023-11-29 20:56:17 -0500

    Added a test for BlockContactInteractor

---
+++ test/Use_case/BlockContactInteractorTest.java
@@ -0,0 +1,46 @@
+package Use_case;
+
+import org.junit.Test;
+import entity.User;
+import entity.CommonUserFactory;
+import data_access.UserDataAccessObject;
+import use_case.block_contact.BlockContactInputData;
+import use_case.block_contact.BlockContactInteractor;
+import use_case.block_contact.BlockContactOutputBoundary;
+
+import static org.junit.Assert.assertFalse;
+
+public class BlockContactInteractorTest {
+
+    @Test
+    public void successTest() {
+        CommonUserFactory commonUserFactory = new CommonUserFactory();
+        User user1 = commonUserFactory.create("user1", "user1");
+        User user2 = commonUserFactory.create("user2", "user2");
+
+        String masterKey = "$2a$10$xfVheBzZjicxu..Dy7zLHeBNVrrPWZ/jEK/qfX7nTY.WKY/Tx9LM2";
+        UserDataAccessObject userDataAccessObject = new UserDataAccessObject(masterKey, commonUserFactory);
+        userDataAccessObject.save(user1);
+        userDataAccessObject.save(user2);
+
+        userDataAccessObject.addFriend("user1", "user2");
+
+        BlockContactInputData blockContactInputData = new BlockContactInputData(user2.getName());
+
+        BlockContactOutputBoundary blockContactPresenter = new BlockContactOutputBoundary() {
+            @Override
+            public void prepareSuccessView() {
+                assertFalse(user1.isFriendWith(user2.getName()));
+                assertFalse(user2.isFriendWith(user1.getName()));
+
+            }
+
+            @Override
+            public void prepareFailView(String error) {
+                System.out.println("Fail to block friend" + error);
+            }
+        };
+        BlockContactInteractor blockContactInteractor = new BlockContactInteractor(blockContactPresenter, userDataAccessObject);
+        blockContactInteractor.execute(blockContactInputData);
+    }
+}
