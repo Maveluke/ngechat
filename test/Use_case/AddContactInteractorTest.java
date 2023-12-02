96ce92697b9504a5623c0218e8ac7875db25c573
Newtella3105 <126584697+Newtella3105@users.noreply.github.com>
2023-12-01 20:28:38 -0500

    Melissa: tests (#38)
    * Added login and signup interface adapters and views
    
    * Added java classes for LoggedIn and edited SignupView.java
    
    * Added java classes for LoggedIn and edited SignupView.java
    
    * Added interface adapters and view for chatlist, and moved some files around.
    
    * Added some icons for ChatListView and uploaded the png photos we need for icons in our views.
    
    * Changed a few minor details in interface adapter and view for ChatList, while also deleting some unnecessary files.
    
    * Implemented FriendsListView (partially with some TODOs), added some TODOs to ChatListView and added files for interface adapters of FriendsList.
    
    * Made some files for inchat, renamed some folders and made some minor changes.
    
    * Changed a variable name
    
    * Changed some things in the views of chatlist, friendslist and inchatprivate
    
    * Changed some things in the views of chatlist and inchatprivate
    
    * Removed some files
    
    * Added png file for sending a message
    
    * Implemented interface adapters and view for FriendsList
    
    * Minor edits to friendslistview
    
    * Minor edits to friendslistview
    
    * Minor edits to FriendsListViewModel
    
    * Implemented BlockContact use case.
    
    * Made some changes in friendslistview to accomodate for BlockContact
    
    * Minor changes
    
    * Minor changes
    
    * Added blockcontact and blockcontactremotely in UserDAO
    
    * Updated the interface adapters of blockcontact and made some changes in friendslistview.
    
    * Minor changes to inchatprivateview
    
    * Change the view visibility
    
    * Made some changes to the interface adapters of block contact and friendslistview
    
    * Added a test for BlockContactInteractor
    
    * Added a file for FriendsListViewTest and started to work on AddContactInteractorTest
    
    * Edited BlockContactInteractorTest, added AddContactInteractorTest and fixed some errors in UserDataAccessObject.
    
    * Added SignupInteractorTest
    
    * Made changes based on timothym4r's comments
    
    * Made changes based on timothym4r's comments
    
    ---------
    
    Co-authored-by: Maveluke <130248289+Maveluke@users.noreply.github.com>
    Co-authored-by: timothym4r <130308583+timothym4r@users.noreply.github.com>

--- test/Use_case/AddContactInteractorTest.java
+++ test/Use_case/AddContactInteractorTest.java
@@ -1,4 +1,42 @@
 package Use_case;
 
+import org.junit.Test;
+import data_access.UserDataAccessObject;
+import entity.CommonUserFactory;
+import use_case.add_contact.AddContactInputData;
+import use_case.add_contact.AddContactInteractor;
+import use_case.add_contact.AddContactOutputBoundary;
+
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
+
 public class AddContactInteractorTest {
+    @Test
+    public void successTest() {
+        CommonUserFactory commonUserFactory = new CommonUserFactory();
+
+        String masterKey = "$2a$10$xfVheBzZjicxu..Dy7zLHeBNVrrPWZ/jEK/qfX7nTY.WKY/Tx9LM2";
+        UserDataAccessObject userDataAccessObject = new UserDataAccessObject(masterKey, commonUserFactory);
+
+        userDataAccessObject.setCurrentUsername("admin");
+
+        AddContactInputData addContactInputData = new AddContactInputData("admin3");
+
+        AddContactOutputBoundary addContactPresenter = new AddContactOutputBoundary() {
+            @Override
+            public void prepareSuccessView() {
+                assertTrue(userDataAccessObject.get("admin").isFriendWith("admin3"));
+                assertTrue(userDataAccessObject.get("admin3").isFriendWith("admin"));
+            }
+
+            @Override
+            public void prepareFailView(String error) {
+                fail("Use case failure is unexpected. ");
+            }
+        };
+        AddContactInteractor addContactInteractor = new AddContactInteractor(addContactPresenter, userDataAccessObject);
+        addContactInteractor.execute(addContactInputData);
+
+        userDataAccessObject.blockFriend("admin", "admin3");
+    }
 }
