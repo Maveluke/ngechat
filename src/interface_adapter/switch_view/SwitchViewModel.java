bcad3b6cbae40353a4e5b857e0f30ed15852489b
Newtella3105 <126584697+Newtella3105@users.noreply.github.com>
2023-12-03 15:28:08 -0500

    Melissa (#49)
    * Added login and signup interface adapters and views
    
    * Added java classes for LoggedIn and edited SignupView.java
    
    * Added java classes for LoggedIn and edited SignupView.java
    
    * Added interface adapters and view for chatlist, and moved some files around.
    
    * Added some icons for ChatListView and uploaded the png photos we need for icons in our views.
    
    * Changed a few minor details in interface adapter and view for ChatList, while also deleting some unnecessary files.
    
    * Implemented FriendsListView (partially with some TODOs), added some TODOs to ChatListView and added files for interface adapters of FriendsList.
    
    * Made some files for inchat, renamed some folders and made some minor changes.
    
    * Changed a variable name
    
    * Changed some things in the views of chatlist, friendslist and inchatprivate
    
    * Changed some things in the views of chatlist and inchatprivate
    
    * Removed some files
    
    * Added png file for sending a message
    
    * Implemented interface adapters and view for FriendsList
    
    * Minor edits to friendslistview
    
    * Minor edits to friendslistview
    
    * Minor edits to FriendsListViewModel
    
    * Implemented BlockContact use case.
    
    * Made some changes in friendslistview to accomodate for BlockContact
    
    * Minor changes
    
    * Minor changes
    
    * Added blockcontact and blockcontactremotely in UserDAO
    
    * Updated the interface adapters of blockcontact and made some changes in friendslistview.
    
    * Minor changes to inchatprivateview
    
    * Change the view visibility
    
    * Made some changes to the interface adapters of block contact and friendslistview
    
    * Added a test for BlockContactInteractor
    
    * Added a file for FriendsListViewTest and started to work on AddContactInteractorTest
    
    * Edited BlockContactInteractorTest, added AddContactInteractorTest and fixed some errors in UserDataAccessObject.
    
    * Added SignupInteractorTest
    
    * Made changes based on timothym4r's comments
    
    * Made changes based on timothym4r's comments
    
    * Edited SignupInteractorTest to change it to SignupTest and made some getters in SignupInputData public
    
    * Renamed AddContactInteractorTest to AddContactTest and made some changes so it tests the controller as well
    
    * Made some changes to AddContactTest and BlockContactInteractorTest while also renaming the latter to BlockContactTest
    
    * Made some changes to AddContactTest and BlockContactInteractorTest while also renaming the latter to BlockContactTest
    
    * Made some changes to LoginInteractorTest and renamed it to LoginTest
    
    * Made small changes to BlockContactTest, AddContactTest, SignupTest and LoginTest
    
    * Added AddContactInteractorTest and SignupInteractorTest
    
    * Deleted some unnecessary imports, methods and files.
    
    * Deleted some unnecessary imports, methods and files and made minor changes to the tests.
    
    * Deleted some unnecessary imports, methods and files and made minor changes to the tests.
    
    * Deleted unnecessary files.
    
    ---------
    
    Co-authored-by: Maveluke <130248289+Maveluke@users.noreply.github.com>
    Co-authored-by: timothym4r <130308583+timothym4r@users.noreply.github.com>

--- src/interface_adapter/switch_view/SwitchViewModel.java
+++
@@ -1,9 +0,0 @@
-package interface_adapter.switch_view;
-
-import interface_adapter.ViewModel;
-
-import java.beans.PropertyChangeListener;
-import java.beans.PropertyChangeSupport;
-
-public class SwitchViewModel{
-}
--- src/interface_adapter/switch_view/SwitchViewState.java
+++
@@ -1,4 +0,0 @@
-package interface_adapter.switch_view;
-
-public class SwitchViewState {
-}
